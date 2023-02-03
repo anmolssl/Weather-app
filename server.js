5ac1f4555b6d114bd738ebd586a0c07c64e49176
anmolssl <anmolssl@outlook.com>
2023-02-03 19:07:30 +0530

    Added locaiton tracking

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const https = require("https");
+const http = require("http");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -7,37 +9,64 @@ const app = express();
 app.use(express.static(__dirname + '/static'));
 app.set("view engine", "ejs");
 
+app.use(bodyParser.json());
 
+http.get("http://ip-api.com/json/", function(response) {
+    console.log(response.statusCode);
 
-app.get("/", function(req, res) {
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Chandigarh,India&appid=0f2b7a5fb36d9e2067a0b8919d60962e&units=metric";
-    
-    https.get(url, function(response) {
-        console.log(response.statusCode);
+    response.on("data", function(data) {
+        locationData = JSON.parse(data);
 
-        response.on('data', function(data) {
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-            var temp = Math.round(weatherData.main.temp);
-            var windSpeed = (weatherData.wind.speed * 3.6).toFixed(1);
-            var pressure = weatherData.main.pressure;
-            var humidity = weatherData.main.humidity;
-            var windDirection = weatherData.wind.deg;
-            var visibility = weatherData.visibility/1000;
+        var lat = locationData.lat;
+        var long = locationData.lon;
 
-            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-            const date = new Date();
-            var day = weekday[date.getDay()];
-            var month = monthNames[date.getMonth()];
-            var todayDate = date.getDate();
-
-            res.render("pages/index.ejs", {temperature: temp, visibility: visibility, windDirection: windDirection, humidity: humidity, pressure: pressure, windSpeed: windSpeed, day: day, month: month, date: todayDate});
+        app.post("/coords", function(req, res) {
+            lat = req.body.latitude;
+            long = req.body.longitude;
+            
         });
-    });
 
-});
+        app.get("/", function(req, res) {
+
+            var url;
+            // "https://api.openweathermap.org/data/2.5/weather?q=" + coords + ",India&appid=0f2b7a5fb36d9e2067a0b8919d60962e&units=metric"
+            // "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=0f2b7a5fb36d9e2067a0b8919d60962e&units=metric"
+            if (lat && long) {
+                url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=0f2b7a5fb36d9e2067a0b8919d60962e&units=metric";
+            } else {
+                url = "https://api.openweathermap.org/data/2.5/weather?q=" + coords + ",India&appid=0f2b7a5fb36d9e2067a0b8919d60962e&units=metric";
+            }
+            
+            https.get(url, function(response) {
+                console.log(response.statusCode);
+        
+                response.on('data', function(data) {
+                    const weatherData = JSON.parse(data);
+                    console.log(weatherData);
+                    var temp = Math.round(weatherData.main.temp);
+                    var windSpeed = (weatherData.wind.speed * 3.6).toFixed(1);
+                    var pressure = weatherData.main.pressure;
+                    var humidity = weatherData.main.humidity;
+                    var windDirection = weatherData.wind.deg;
+                    var visibility = weatherData.visibility/1000;
+                    var cityName = weatherData.name;
+                    var countryCode = weatherData.sys.country;
+        
+                    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    const date = new Date();
+                    var day = weekday[date.getDay()];
+                    var month = monthNames[date.getMonth()];
+                    var todayDate = date.getDate();
+        
+                    res.render("pages/index.ejs", {weatherLocation: cityName + ", " + countryCode, temperature: temp, visibility: visibility, windDirection: windDirection, humidity: humidity, pressure: pressure, windSpeed: windSpeed, day: day, month: month, date: todayDate});
+                });
+            });
+        
+        });
+
+    })
+})
 
 app.listen(PORT, function() {
     console.log("Server started on 3000");
--- static/index.js
+++ static/index.js
@@ -1,4 +1,6 @@
 
+// Search for places funciton
+
 $(".search-button").click( function() {
     $(".search-container").css("visibility", "visible");
     $("body").css("overflow", "hidden");
@@ -9,8 +11,60 @@ $(".search-container span").click( function() {
     $("body").css("overflow", "overlay");
 });
 
+// updating humidty percentage
+
 var humidityPercentage =  $(".percentage-barfilled").attr("value");
 $(".percentage-barfilled").css("width", humidityPercentage + "%")
 
+// updating wind direction
+
 var windDirectionDeg = $(".wind-direction-sign").attr("value");
 $(".wind-direction-sign").css("transform", "rotate(" + windDirectionDeg + "deg)");
+
+// On click to fetch user location coords
+
+$(".my-location-btn").click( function() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Your browser doesn't support location tracking");
+    }
+});
+
+
+// --------------------Functions Declaring-------------------
+
+// Fetching location coords from browser
+
+function onSuccess(position) {
+    coordsDetails = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    }
+    sendingCoordsToServer(coordsDetails);
+    location.reload();
+}
+
+function onError(error) {
+    if(error.code === 1) {
+        alert("You denied location request");
+    } else if (error.code === 2) {
+        alert("Location not found");
+    } else {
+        alert("Something went wrong!");
+    }
+    $(".my-location-btn").attr("disabled", "true");
+}
+
+// Sending location coords to Server
+
+function sendingCoordsToServer(object) {
+    var jsonstring = JSON.stringify(object);
+
+    var xhr = new XMLHttpRequest;
+
+    xhr.open('POST', '/coords', true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(jsonstring);
+    console.log("Coords sent to server");
+}
